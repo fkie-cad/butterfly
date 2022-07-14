5c56f81bb5a5e96f2607788a951aa42b8553f0a8
pd-fkie <77979557+pd-fkie@users.noreply.github.com>
2022-07-14 23:10:57 +0200

    Added more compile tests

--- src/lib.rs
+++ src/lib.rs
@@ -221,8 +221,7 @@ mod tests {
         }
     }
 
-    #[test]
-    #[ignore]
+    #[allow(dead_code)]
     fn multicore_harness() {
         let shmem_provider = StdShMemProvider::new().unwrap();
         let mon = FuzzerStatsWrapper::new(StateMonitor::new(), "fuzzer_stats", 30);
@@ -256,8 +255,7 @@ mod tests {
         launcher.launch().unwrap();
     }
 
-    #[test]
-    #[ignore]
+    #[allow(dead_code)]
     fn singlecore_harness() {
         let mon = FuzzerStatsWrapper::new(StateMonitor::new(), "fuzzer_stats", 30);
         let mut mgr = SimpleEventManager::new(mon);
@@ -278,6 +276,127 @@ mod tests {
         ));
         let mut stages = tuple_list!(StdMutationalStage::new(mutator));
         let mut executor = ExampleExecutor::new(tuple_list!(state_observer));
+        fuzzer.fuzz_loop(&mut stages, &mut executor, &mut state, &mut mgr).unwrap();
+    }
+    
+    #[derive(Hash, Debug, Clone, Serialize, Deserialize)]
+    struct RawInput {
+        packets: Vec<BytesInput>,
+    }
+    impl<MT, S> HasHavocMutations<MT, S> for RawInput
+    where
+        MT: MutatorsTuple<BytesInput, S>,
+        S: HasRand,
+    {
+        fn mutate_packet(&mut self, packet: usize, mutations: &mut MT, mutation: usize, state: &mut S, stage_idx: i32) -> Result<MutationResult, Error> {
+            mutations.get_and_mutate(mutation, state, &mut self.packets[packet], stage_idx)
+        }
+    }
+    impl Input for RawInput {
+        fn generate_name(&self, _idx: usize) -> String {
+            todo!();
+        }
+    }
+    impl HasPackets<BytesInput> for RawInput {
+        fn packets(&self) -> &[BytesInput] {
+            &self.packets
+        }
+
+        fn packets_mut(&mut self) -> &mut Vec<BytesInput> {
+            &mut self.packets
+        }
+    }
+    impl HasLen for RawInput {
+        fn len(&self) -> usize {
+            self.packets.len()
+        }
+    }
+    impl HasPcapRepresentation<RawInput> for RawInput {
+        fn from_pcap(mut _capture: Capture<Offline>) -> Result<Self, Error> {
+            todo!();
+        }
+    }
+    
+    struct RawExecutor<OT, S>
+    where
+        OT: ObserversTuple<RawInput, S>,
+    {
+        observers: OT,
+        phantom: PhantomData<S>,
+    }
+    impl<OT, S> RawExecutor<OT, S>
+    where
+        OT: ObserversTuple<RawInput, S>,
+    {
+        fn new(observers: OT) -> Self {
+            Self {
+                observers,
+                phantom: PhantomData,
+            }
+        }
+    }
+    impl<OT, S> Debug for RawExecutor<OT, S>
+    where
+        OT: ObserversTuple<RawInput, S>,
+    {
+        fn fmt(&self, _f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+            todo!();
+        }
+    }
+    impl<OT, S, EM, Z> Executor<EM, RawInput, S, Z> for RawExecutor<OT, S>
+    where
+        OT: ObserversTuple<RawInput, S>,
+    {
+        fn run_target(&mut self, _fuzzer: &mut Z, _state: &mut S, _mgr: &mut EM, input: &RawInput) -> Result<ExitKind, Error> {
+            let state_observer: &mut StateObserver<TargetState> = self.observers.match_name_mut("state").unwrap();
+
+            for _packet in &input.packets {
+                // do some I/O with packet data
+
+                // the executor is responsible for getting state information
+                // from the target
+                let current_state = TargetState::default();
+
+                state_observer.record(&current_state);
+            }
+
+            Ok(ExitKind::Ok)
+        }
+    }
+    impl<OT, S> HasObservers<RawInput, OT, S> for RawExecutor<OT, S>
+    where
+        OT: ObserversTuple<RawInput, S>,
+    {
+        fn observers(&self) -> &OT {
+            &self.observers
+        }
+
+        fn observers_mut(&mut self) -> &mut OT {
+            &mut self.observers
+        }
+    }
+    
+    #[allow(dead_code)]
+    fn raw_harness() {
+        let mon = FuzzerStatsWrapper::new(StateMonitor::new(), "fuzzer_stats", 30);
+        let mut mgr = SimpleEventManager::new(mon);
+        let state_observer = StateObserver::<TargetState>::new("state");
+        let mut feedback = StateFeedback::new(&state_observer);
+        let mut objective = CrashFeedback::new();
+        let mut state = StdState::new(StdRand::with_seed(0), InMemoryCorpus::new(), InMemoryCorpus::new(), &mut feedback, &mut objective).unwrap();
+        let scheduler = QueueScheduler::new();
+        let mut fuzzer = StdFuzzer::new(scheduler, feedback, objective);
+        let mutator = PacketMutationScheduler::new(tuple_list!(
+            PacketHavocMutator::new(supported_havoc_mutations()),
+            PacketReorderMutator::new(),
+            PacketSpliceMutator::new(4),
+            PacketCrossoverInsertMutator::new(),
+            PacketCrossoverReplaceMutator::new(),
+            PacketDeleteMutator::new(4),
+            PacketDuplicateMutator::new(16)
+        ));
+        let mut stages = tuple_list!(StdMutationalStage::new(mutator));
+        let mut executor = RawExecutor::new(tuple_list!(state_observer));
         fuzzer.fuzz_loop(&mut stages, &mut executor, &mut state, &mut mgr).unwrap();
     }
 }
